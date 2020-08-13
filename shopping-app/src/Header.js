10da7af8da05eea7011719c2402e3fe7330979a8
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-08-13 17:04:47 +0300

    created Header component

---
+++ shopping-app/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div style={{backgroundColor: '#333', height: 200}}>
+            <h1 style={{color: 'white', textAlign: 'center', paddingTop:150 }}>Hello Basket!</h1>
+        </div>
+    )
+    
+}
+
+export default Header;
