ddf6ddc6b0ba9a3e378cbd59ae1bc845e0d74111
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-08-14 12:45:54 +0300

    minor fix

--- shopping-app/src/SearchBar.js
+++ shopping-app/src/SearchBar.js
@@ -4,7 +4,7 @@ function SearchBar(props) {
     
     return (
         <div>
-            <input onChange={(e) => {props.search(e.target.value.toLowerCase())}} placeholder="Search in List" type="text"></input>
+            <input onChange={(e) => {props.search(e.target.value.toLowerCase())}} placeholder="Search in groceries list" type="text"></input>
         </div>
     )
 }
