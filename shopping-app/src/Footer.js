3b2bc2a666c8450212798cb63a01526749a38eed
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-08-15 16:57:27 +0300

    added created by

--- shopping-app/src/Footer.js
+++ shopping-app/src/Footer.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div style={{ backgroundColor: '#222', height: 150, }}>
-            <p style={{ textAlign: 'center', paddingTop: 50, margin: 0 }}>
+        <div style={{ backgroundColor: '#222', height: 150, textAlign: 'center'}}>
+            <p style={{ paddingTop: 50, margin: 0 }}>
                 <a style={{color: 'white'}} href='#root'>All</a>, <a style={{color: 'white'}} href='#root'>Pending</a>, <a style={{color: 'white'}} href='#root'>Purchased</a>
-                </p>
+            </p>
+            <p style={{color: 'white', fontFamily: 'Courier New'}}>Created By Amir Debbie.</p>
         </div>
     );
 }
--- shopping-app/src/Header.js
+++ shopping-app/src/Header.js
@@ -4,7 +4,7 @@ import cartIcon from './cart-icon.png'
 function Header() {
     return (
         <div style={{backgroundColor: '#222', height: 200, textAlign:'center'}}>
-            <img style={{height: 100, paddingTop: 40, paddingBottom: 10}} alt='Basket' src={cartIcon} />
+            <img style={{height: 100, paddingTop: 30, paddingBottom: 10}} alt='Basket' src={cartIcon} />
             <h1 style={{fontSize: 25, margin: 0, color: 'white', textAlign: 'center'}}>Hello, Basket</h1>
         </div>
     )
